34d56fe7f757f1a4065ffabfc00079e46690b544
NayenRC <nay.roman@duocuc.cl>
2025-11-09 20:42:59 -0300

    base-api

---
+++ src/main/java/Ecommerce_FullStackcom/example/Ecommerce/Service/TiendaService.java
@@ -0,0 +1,34 @@
+package Ecommerce_FullStackcom.example.Ecommerce.Service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import Ecommerce_FullStackcom.example.Ecommerce.model.Tienda;
+import Ecommerce_FullStackcom.example.Ecommerce.repository.TiendaRepository;
+
+import java.util.List;
+@Service
+public class TiendaService {
+     @Autowired
+   private TiendaRepository tiendaRepository;
+
+   public List<Tienda> getAllTiendas() {
+       return tiendaRepository.findAll();
+   }
+
+   public Tienda getTiendaById(Long id) {
+       return tiendaRepository.findById(id).orElse(null);
+   }
+
+   public Tienda saveTienda(Tienda tienda) {
+       return tiendaRepository.save(tienda);
+   }
+
+   public void deleteTienda(Long id) {
+       tiendaRepository.deleteById(id);
+   }
+
+  
+
+
+}
---
+++ src/main/java/Ecommerce_FullStackcom/example/Ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package Ecommerce_FullStackcom.example.Ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SwaggerConfig {
+    
+ 
+
+   @Bean
+   public OpenAPI customOpenAPI(){
+       return new OpenAPI().info(
+           new Info()
+           .title("API demo")
+           .version("0.1")
+           .description("Api de Tienda Online")
+       );
+   }
+   
+
+}
