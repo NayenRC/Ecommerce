f0205159f45ac89cd12a1a1820050a7fc97d98ae
AllanxSF <al.salinast@duocuc.cl>
2025-11-11 01:09:55 -0300

    repository

--- src/main/java/Ecommerce_FullStackcom/example/Ecommerce/repository/RegionRepository.java
+++ src/main/java/Ecommerce_FullStackcom/example/Ecommerce/repository/RegionRepository.java
@@ -1,5 +1,11 @@
 package Ecommerce_FullStackcom.example.Ecommerce.repository;
 
-public class RegionRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Ecommerce_FullStackcom.example.Ecommerce.model.Region;
+
+@Repository
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+    // Ejemplo: List<Region> findByNombreRegionContaining(String texto);
 }
--- src/main/java/Ecommerce_FullStackcom/example/Ecommerce/repository/UsuarioRepository.java
+++ src/main/java/Ecommerce_FullStackcom/example/Ecommerce/repository/UsuarioRepository.java
@@ -1,5 +1,13 @@
 package Ecommerce_FullStackcom.example.Ecommerce.repository;
 
-public class UsuarioRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Ecommerce_FullStackcom.example.Ecommerce.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    // Aquí puedes agregar consultas personalizadas si las necesitas
+    // Ejemplo:
+    // Optional<Usuario> findByCorreoElectronico(String correoElectronico);
 }
