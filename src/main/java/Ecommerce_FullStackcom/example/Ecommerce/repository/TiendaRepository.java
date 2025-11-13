270f24b159e5b9e2186b3eebf7251b04f1d78fd2
AllanxSF <al.salinast@duocuc.cl>
2025-11-13 12:45:23 -0300

    tienda borrado

--- src/main/java/Ecommerce_FullStackcom/example/Ecommerce/repository/TiendaRepository.java
+++
@@ -1,8 +0,0 @@
-package Ecommerce_FullStackcom.example.Ecommerce.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Ecommerce_FullStackcom.example.Ecommerce.model.Tienda;
-
-public interface TiendaRepository extends JpaRepository<Tienda, Long> {
-
-}
