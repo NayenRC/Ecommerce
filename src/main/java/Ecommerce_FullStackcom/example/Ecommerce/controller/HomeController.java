24a18c52b1fc8bab33b9240566687f343bc3e84f
AllanxSF <al.salinast@duocuc.cl>
2025-11-16 01:36:25 -0300

    home

---
+++ src/main/java/Ecommerce_FullStackcom/example/Ecommerce/controller/HomeController.java
@@ -0,0 +1,17 @@
+package Ecommerce_FullStackcom.example.Ecommerce.controller;
+
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public void redirectToSwagger(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+}
