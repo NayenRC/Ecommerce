17285939375c3dd630b3a500ecd6a76cf5e7b08d
AllanxSF <al.salinast@duocuc.cl>
2025-11-15 23:25:45 -0300

    Docker

--- src/main/java/Ecommerce_FullStackcom/example/Ecommerce/config/SecurityConfig.java
+++ src/main/java/Ecommerce_FullStackcom/example/Ecommerce/config/SecurityConfig.java
@@ -2,11 +2,34 @@ package Ecommerce_FullStackcom.example.Ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // 🔓 LIBERAR SWAGGER
+                        .requestMatchers(
+                                "/doc/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml")
+                        .permitAll()
+                        // 🔐 EL RESTO REQUIERE AUTENTICACIÓN
+                        .anyRequest().authenticated())
+                .formLogin(form -> form.permitAll());
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
