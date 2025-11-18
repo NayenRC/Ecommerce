6d65300c1f1a1ada6c44f77fbbb196012d1605c0
AllanxSF <al.salinast@duocuc.cl>
2025-11-18 17:05:47 -0300

    sec

--- src/main/java/Ecommerce_FullStackcom/example/Ecommerce/config/SecurityConfig.java
+++ src/main/java/Ecommerce_FullStackcom/example/Ecommerce/config/SecurityConfig.java
@@ -1,8 +1,9 @@
 package Ecommerce_FullStackcom.example.Ecommerce.config;
 
+import java.beans.Customizer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,19 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults()); // 🔥 ESTA LÍNEA ES LA QUE TE FALTABA
-
-        return http.build();
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
