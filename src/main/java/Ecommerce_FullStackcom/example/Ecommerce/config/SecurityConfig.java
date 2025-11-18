a86b0fd0ad9b8bffb6f5c9a8303cba476dc373a7
AllanxSF <al.salinast@duocuc.cl>
2025-11-18 17:26:54 -0300

    Back Final

--- src/main/java/Ecommerce_FullStackcom/example/Ecommerce/config/SecurityConfig.java
+++ src/main/java/Ecommerce_FullStackcom/example/Ecommerce/config/SecurityConfig.java
@@ -1,11 +1,7 @@
 package Ecommerce_FullStackcom.example.Ecommerce.config;
 
-import java.beans.Customizer;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
