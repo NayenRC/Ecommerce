265c95ae6ae69a2b32a555423ab6a8c99d02ec12
AllanxSF <al.salinast@duocuc.cl>
2025-12-10 00:46:57 -0300

    login5

--- src/main/java/Ecommerce_FullStackcom/example/Ecommerce/security/JwtUtil.java
+++ src/main/java/Ecommerce_FullStackcom/example/Ecommerce/security/JwtUtil.java
@@ -1,32 +1,35 @@
 package Ecommerce_FullStackcom.example.Ecommerce.security;
 
-import java.util.Date;
-
-import javax.crypto.SecretKey;
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.Date;
+
 @Component
 public class JwtUtil {
 
-    private final SecretKey SECRET_KEY = Keys.hmacShaKeyFor("12345678901234567890123456789012".getBytes());
-    private final long EXPIRATION_TIME = 86400000; // 24h
+    private final String SECRET = "12345678901234567890123456789012"; // 32 chars
+    private final long EXPIRATION_TIME = 86400000; // 24 horas
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public String getEmailFromToken(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(SECRET_KEY)
+                .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
